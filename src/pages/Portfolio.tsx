f6b9c7e473ea98ea334afe5ae1a95f60120f6358
tom <tom@tom-iMac.local>
2025-09-05 17:47:56 +0200

    update

--- src/pages/Portfolio.tsx
+++ src/pages/Portfolio.tsx
@@ -1,19 +1,7 @@
 import { useTranslation } from 'react-i18next';
 import { ProjectList } from '../components/Portfolio/ProjectList';
 import Heading from '../components/Heading';
-import styled from 'styled-components';
 import { PageMeta } from '../components/PageMeta';
-
-const Wrapper = styled.div`
-h1{
-  font-size: 600%;
-  font-weight: 100;
-  letter-spacing: -.04em;
-  text-align: center;
-  line-height:2;
-  margin:0;
-}
-`;
 
 export const Portfolio = () => {
   const { t } = useTranslation();
@@ -24,11 +12,10 @@ export const Portfolio = () => {
         title={`${t('portfolio.title')} - ${t('common.title')}`}
         description={`${t('portfolio.title')} - ${t('common.title')}`}
       />
-    
-      <Wrapper className="content portfolio">
+      <div className="content portfolio">
         <Heading>{t('portfolio.title')}</Heading>
         <ProjectList />
-      </Wrapper>
+      </div>
     </>
   );
 };
