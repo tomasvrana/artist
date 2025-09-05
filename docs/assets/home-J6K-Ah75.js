b3d2f34c397a3e849559865a11df7394d60c6a9f
tom <tom@tom-iMac.local>
2025-09-05 03:13:47 +0200

    update

--- docs/assets/home-J6K-Ah75.js
+++ docs/assets/home-J6K-Ah75.js
@@ -6,7 +6,7 @@ gallery:
   - image: /images/gayvasion/0.jpg
   - image: /images/hospital/1.jpg
   - image: /images/attraction/8.jpg
-  - image: /images/monday/1.jpg
+  - image: /images/monday/2.jpg
   - image: /images/kolos/1.jpg
 ---
 `;export{a as default};
--- src/content/pages/cs/home.md
+++ src/content/pages/cs/home.md
@@ -6,6 +6,6 @@ gallery:
   - image: /images/gayvasion/0.jpg
   - image: /images/hospital/1.jpg
   - image: /images/attraction/8.jpg
-  - image: /images/monday/1.jpg
+  - image: /images/monday/2.jpg
   - image: /images/kolos/1.jpg
 ---
